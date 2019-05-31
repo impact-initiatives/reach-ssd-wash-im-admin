22ed35846685f91030ebca83e00367bcc5f08a49
Max Malynowsky <maxmalynowsky@gmail.com>
2019-05-31 19:57:10 +1000

    add field for new password for new users

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -2,43 +2,79 @@ import React, { useState } from 'react';
 import Auth from '@aws-amplify/auth';
 import { FaLock, FaEnvelope } from 'react-icons/fa';
 
+interface State {
+  loading: boolean;
+  usernameErr: boolean;
+  passwordErr: boolean;
+  challengeName: string;
+}
+
 const handleSubmit = (
   e: React.FormEvent<HTMLFormElement>,
   setLoading: Function,
 ) => {
   e.preventDefault();
-  setLoading({ loading: true, usernameErr: false, passwordErr: false });
-  const values = { email: '', password: '' };
+  setLoading((state: State) => ({
+    ...state,
+    loading: true,
+    usernameErr: false,
+    passwordErr: false,
+  }));
+  const values = { email: '', password: '', newPassword: '' };
   for (const input of Array.from(e.currentTarget.elements)) {
     const { name, value } = input as HTMLInputElement;
     if (name === 'email') values.email = value;
     if (name === 'password') values.password = value;
+    if (name === 'newPassword') values.newPassword = value;
   }
   Auth.signIn(values.email, values.password)
     .then(user => {
-      console.log(user);
-      if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
-        Auth.completeNewPassword(user, values.password, {});
-      } else window.location.assign('/');
+      if (user.challengeName === 'NEW_PASSWORD_REQUIRED' && values.newPassword)
+        Auth.completeNewPassword(user, values.password, {}).then(() =>
+          window.location.assign('/'),
+        );
+      else if (user.challengeName)
+        setLoading((state: State) => ({
+          ...state,
+          loading: false,
+          challengeName: user.challengeName,
+        }));
+      else window.location.assign('/');
     })
     .catch(({ code }) => {
       if (code === 'UserNotFoundException')
-        setLoading({ loading: false, usernameErr: true, passwordErr: false });
+        setLoading((state: State) => ({
+          ...state,
+          loading: false,
+          usernameErr: true,
+        }));
       else if (code === 'NotAuthorizedException')
-        setLoading({ loading: false, usernameErr: false, passwordErr: true });
-      else setLoading({ loading: false, usernameErr: true, passwordErr: true });
+        setLoading((state: State) => ({
+          ...state,
+          loading: false,
+          passwordErr: true,
+        }));
+      else
+        setLoading((state: State) => ({
+          ...state,
+          loading: false,
+          usernameErr: true,
+          passwordErr: true,
+        }));
     });
 };
 
-const NormalLoginForm = () => {
+const LoginForm = () => {
   const [state, setState] = useState({
     loading: false,
     usernameErr: false,
     passwordErr: false,
+    challengeName: '',
   });
   const loadingClass = state.loading ? ' is-loading' : '';
   const usernameErrClass = state.usernameErr ? 'help is-danger' : 'is-hidden';
   const passwordErrClass = state.passwordErr ? 'help is-danger' : 'is-hidden';
+  console.log(state.challengeName);
   return (
     <form onSubmit={e => handleSubmit(e, setState)}>
       <div className="field">
@@ -77,6 +113,26 @@ const NormalLoginForm = () => {
         </p>
         <p className={passwordErrClass}>Password is incorrect</p>
       </div>
+      {state.challengeName === 'NEW_PASSWORD_REQUIRED' ? (
+        <div className="field">
+          <p className="control has-icons-left">
+            <input
+              className="input"
+              type="password"
+              name="newPassword"
+              placeholder="New password"
+              autoComplete="new-password"
+              required={true}
+              aria-required="true"
+              aria-label="New password"
+            />
+            <span className="icon is-small is-left">
+              <FaLock />
+            </span>
+          </p>
+          <p className={passwordErrClass}>Password is incorrect</p>
+        </div>
+      ) : null}
       <div className="field">
         <p className="control">
           <button className={`button is-primary is-rounded ${loadingClass}`}>
@@ -88,4 +144,4 @@ const NormalLoginForm = () => {
   );
 };
 
-export default NormalLoginForm;
+export default LoginForm;
