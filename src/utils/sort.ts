9a3c41570f996ad3233ae532809d60023ad58d60
Max Malynowsky <maxmalynowsky@gmail.com>
2019-10-28 14:58:07 -0600

    sort documents by date, type, name

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -1,6 +1,8 @@
 const sort = (a: Data, b: Data): number => {
   if (a.endDate > b.endDate) return -1;
   if (a.endDate < b.endDate) return 1;
+  if (a.documentType < b.documentType) return -1;
+  if (a.documentType > b.documentType) return 1;
   if (a.title < b.title) return -1;
   if (a.title > b.title) return 1;
   return 0;
